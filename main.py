19ca5412f5f125db0ee2660549a5e71841a0d678
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2023-12-01 11:15:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,28 +27,40 @@ class Contacto(BaseModel):
     email : str
     nombre : str
     telefono : str
+@app.post("/contactos")
+async def crear_contacto(contacto: Contacto):
+    try:
+        # Verificar si el contacto ya existe
+        c = conn.cursor()
+        c.execute('SELECT COUNT(*) FROM contactos WHERE email = ?', (contacto.email,))
+        count = c.fetchone()[0]
+
+        if count > 0:
+            # El contacto ya existe, devolver un error
+            raise HTTPException(status_code=400, detail="El contacto ya existe")
+
+        # Si no hay duplicados, proceder con la inserción
+        c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+                  (contacto.email, contacto.nombre, contacto.telefono))
+        conn.commit()
+        
+        return contacto
+    except Exception as e:
+        logging.error(str(e))
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
 
 @app.get("/contactos")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
-    # DONE Consulta todos los contactos de la base de datos y los envia en un JSON
+    # TODO Consulta todos los contactos de la base de datos y los envia en un JSON
     c = conn.cursor()
-    c.execute('SELECT * FROM contactos')
+    c.execute('SELECT * FROM contactos;')
     response = []
     for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
+        contacto = {"email":row[0],"nombre":row[1], "telefono":row[2]}
         response.append(contacto)
     return response
 
-@app.post("/contactos")
-async def crear_contacto(contacto: Contacto):
-    """Crea un nuevo contacto."""
-    # DONE Inserta el contacto en la base de datos y responde con un mensaje
-    c = conn.cursor()
-    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
-              (contacto.email, contacto.nombre, contacto.telefono))
-    conn.commit()
-    return contacto
 
 @app.get("/contactos/{email}")
 async def obtener_contacto(email: str):
@@ -58,13 +70,13 @@ async def obtener_contacto(email: str):
     c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
     contacto = None
     for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
+        contacto = {"email":row[0],"nombre":row[1],"telefono":row[2]}
     return contacto
+
 
 @app.put("/contactos/{email}")
 async def actualizar_contacto(email: str, contacto: Contacto):
     """Actualiza un contacto."""
-    # DONE Actualiza el contacto en la base de datos
     c = conn.cursor()
     c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
               (contacto.nombre, contacto.telefono, email))
@@ -74,8 +86,8 @@ async def actualizar_contacto(email: str, contacto: Contacto):
 @app.delete("/contactos/{email}")
 async def eliminar_contacto(email: str):
     """Elimina un contacto."""
-    # DONE Elimina el contacto de la base de datos
+    # TODO Elimina el contacto de la base de datos
     c = conn.cursor()
     c.execute('DELETE FROM contactos WHERE email = ?', (email,))
     conn.commit()
-    return {"mensaje":"Elemento borrado"}
+    return {"mensaje":"Contacto eliminado"}
